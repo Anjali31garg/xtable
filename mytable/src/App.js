ab05bb1569cc0c27131e5ac14c3fe53d61ef14fb
Anjali Garg <anjaligarg@dentalkart.com>
2024-05-01 04:43:49 +0530

    push

---
+++ mytable/src/App.js
@@ -0,0 +1,84 @@
+import React, {useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const initialData = 
+[
+
+    { date: "2022-09-01", views: 100, article: "Article 1" },
+
+    { date: "2023-09-01", views: 100, article: "Article 1" },
+
+    { date: "2023-09-02", views: 150, article: "Article 2" },
+
+    { date: "2023-09-02", views: 120, article: "Article 3" },
+
+    { date: "2020-09-03", views: 200, article: "Article 4" }
+
+]
+
+
+
+
+function App() {
+const[tableData, setTableData] = useState(initialData);
+
+
+
+  const dateSort = ()=>{
+    const sortedData = [...tableData].sort((a,b)=>{
+      if(a.date === b.date){
+        return b.views- a.views;
+      }
+      return new Date(b.date) - new Date(a.date);
+    })
+    setTableData(sortedData);
+  }
+
+  const viewSort =()=>{
+    const sortedData = [...tableData].sort((a,b)=>{
+      if(a.views === b.views){
+        return new Date(b.date) - new Date(a.date);
+      }
+
+      return b.views - a.views;
+    })
+
+    setTableData(sortedData);
+  }
+
+
+  return (
+    <div className="App">
+    <h1>Date and Views Table</h1>
+    <div className='buttons'>
+    <button onClick={dateSort}>Sort by Date</button>
+    <button onClick={viewSort}>Sort by Views</button>
+    </div>
+    <div className='table'>
+    <table>
+      <thead>
+        <tr>
+        <th>Date</th>
+            <th>Views</th>
+            <th>Article</th>
+        </tr>
+      </thead>
+      <tbody>
+      {[...tableData].map((row,i)=>{
+        return(
+          <tr key={i}>
+              <td>{row.date}</td>
+              <td>{row.views}</td>
+              <td>{row.article}</td>
+          </tr>
+        )
+      })}
+      </tbody>
+    </table>
+</div>
+    </div>
+  );
+}
+
+export default App;
